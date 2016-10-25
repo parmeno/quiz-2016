3f0aa6c3277700419c1a1b07abf7d4ddef606c77
parmeno <angelparmeno@gmail.com>
2016-10-25 08:32:56 +0200

    identificacion de version

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var quizController = require('../controllers/quiz_controller.js')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz2016' });
 });
+
+router.get('/quizes/question', quizController.question);
+router.get('/quizes/answer', quizController.answer);
 
 module.exports = router;
